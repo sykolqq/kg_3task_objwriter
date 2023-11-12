792a42caa4f42bcb670ba8cbcfff8b2c5d12aae0
pan0mera <artess.ponomarev@mail.ru>
2023-11-12 21:30:46 +0300

    Клонировать папку с гита с objReader и созал базовую структуру для своей разработки

---
+++ ObjWritter/src/com/cgvsu/objWritter/ObjWritter.java
@@ -0,0 +1,15 @@
+package com.cgvsu.objWritter;
+
+import com.cgvsu.model.Model;
+
+public class ObjWritter {
+
+    private static final String OBJ_VERTEX_TOKEN = "v";
+    private static final String OBJ_TEXTURE_TOKEN = "vt";
+    private static final String OBJ_NORMAL_TOKEN = "vn";
+    private static final String OBJ_FACE_TOKEN = "f";
+
+    public void saveModel(Model model, String path){
+
+    }
+}
